90433557b273e04824b0c2bc830ad83a8237565f
briosoemilio <brioso.emilioluis@gmail.com>
2024-10-24 10:07:39 +0800

    Add validation to update-item dto

--- src/item/dto/update-item.dto.ts
+++ src/item/dto/update-item.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateItemDto } from './create-item.dto';
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { ItemStatus } from '@prisma/client';
 
 export class ChangeItemStatusDto extends PartialType(CreateItemDto) {
   @IsNotEmpty()
+  @IsEnum(ItemStatus)
   status: ItemStatus;
 }
 
