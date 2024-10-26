90184818e01164f3be452028ebb2ac438bf84931
briosoemilio <brioso.emilioluis@gmail.com>
2024-10-26 22:48:19 +0800

    Relocate strategy files

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { Payload } from './types';
+import { Payload } from '../types';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "process.env.JWT_TOKEN",
+      secretOrKey: 'process.env.JWT_SECRET',
     });
   }
 
